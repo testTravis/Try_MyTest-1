86d2dbae2d6466be567ff8d802794a70a7b6ac51
sozeri <shira.ozeri@mobiletornado.com>
2017-05-16 15:19:30 +0300

    try2

--- app/src/test/java/com/example/sozeri/try_mytest/ExampleUnitTest.java
+++ app/src/test/java/com/example/sozeri/try_mytest/ExampleUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**  
+/**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
@@ -17,5 +17,10 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect1() throws Exception {
         assertEquals(5, 3 + 2);
+    }
+
+    @Test
+    public void addition_isCorrect2() throws Exception {
+        assertEquals(2, 3 + 2);
     }
 }
